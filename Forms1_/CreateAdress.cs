0e055312f19ffb2978dc6556b3eb11dfc3b413ea
muril0ve <murilo.coutinho@aluno.ifsp.edu.br>
2023-11-16 11:12:53 -0300

    dasd

--- Forms1_/CreateAdress.cs
+++ Forms1_/CreateAdress.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -33,86 +34,65 @@ namespace Forms1_
         private void button1_Click(object sender, EventArgs e)
         {
             Adress adress = new Adress(Id, rua.Text, numero.Text, bairro.Text, cidade.Text, estado.Text, telefone.Text);
-            AdressDAO nomeDoObj = new AdressDAO();
-            nomeDoObj.InsertAdress(adress);
+            string numeroTelefone = adress.Telefone.Replace("(", "").Replace(")", "").Replace("-", "").Replace(" ", "");
+            bool valido = ValidarNumeroTelefone(numeroTelefone);
+            if (valido)
+            {
 
-            string arua = rua.Text;
-            string anumero = numero.Text;
-            string abairro = bairro.Text;
-            string acidade = cidade.Text;
-            string aestado = estado.Text;
-            string atelefone = telefone.Text;
+                AdressDAO nomeDoObj = new AdressDAO();
+                nomeDoObj.InsertAdress(adress);
 
-            string message = "Rua: " + arua +
-                         "\nNumero: " + anumero +
-                         "\nBairro: " + abairro +
-                         "\nCidade: " + acidade +
-                         "\nEstado: " + aestado +
-                         "\nTelefone: " + atelefone;
+                string arua = rua.Text;
+                string anumero = numero.Text;
+                string abairro = bairro.Text;
+                string acidade = cidade.Text;
+                string aestado = estado.Text;
+                string atelefone = telefone.Text;
+
+                string message = "Rua: " + arua +
+                             "\nNumero: " + anumero +
+                             "\nBairro: " + abairro +
+                             "\nCidade: " + acidade +
+                             "\nEstado: " + aestado +
+                             "\nTelefone: " + atelefone;
 
 
-            MessageBox.Show(
-                "Conta criada com sucesso!!",
+                MessageBox.Show(
+                    "Conta criada com sucesso!!",
+                    "ATENÇÃO",
+                MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+
+
+                rua.Clear();
+                bairro.Clear();
+                cidade.Clear();
+                estado.Clear();
+                telefone.Clear();
+                numero.Clear();
+
+            }
+            else
+            {
+                MessageBox.Show(
+                "O número de telefone é inválido!",
                 "ATENÇÃO",
-            MessageBoxButtons.OK,
-                MessageBoxIcon.Information);
-
-
-            rua.Clear();
-            bairro.Clear();
-            cidade.Clear();
-            estado.Clear();
-            telefone.Clear();
-            numero.Clear();
-
-            // Obter a string da TextBox
-            string input = telefone.Text;
-
-            // Remover caracteres não numéricos
-            string numerosApenas = RemoverNaoNumericos(input);
-
-            // Inserir no banco de dados
-            InserirNoBancoDeDados(numerosApenas);
-        }
-        private string RemoverNaoNumericos(string input)
-        {
-            // Utilizar expressão regular para manter apenas os dígitos
-            return Regex.Replace(input, @"[^\d]", "");
-        }
-
-        private void InserirNoBancoDeDados(string numerosApenas)
-        {
-            try
-            {
-                string servidor = "DESKTOP-MBDNUGB\\SQLEXPRESS"; // substitua pelo endereço do seu servidor de banco de dados
-                string bancoDeDados = "PR2";
-               
-
-                string connectionString = $"Data Source={servidor};Initial Catalog={bancoDeDados}";
-
-                
-
-                using (SqlConnection conexao = new SqlConnection(connectionString))
-                {
-                    conexao.Open();
-
-                    // Substitua "NomeDaTabela" pelo nome real da tabela no seu banco de dados
-                    string query = $"INSERT INTO adress (@telefone) VALUES ('{numerosApenas}')";
-
-                    using (SqlCommand comando = new SqlCommand(query, conexao))
-                    {
-                        comando.ExecuteNonQuery();
-                    }
-                }
-
-                MessageBox.Show("Dados inseridos no banco de dados com sucesso!");
-                Close();
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information) ;
             }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Erro ao inserir no banco de dados: {ex.Message}");
-            }
+
+
+
         }
+        static bool ValidarNumeroTelefone(string numero)
+        {
+            string padrao = @"^([1-9]{2}|[1-9]{2})9\d{8}$";
+
+            return Regex.IsMatch(numero, padrao);
+        }
+
+
+
 
         private void button2_Click(object sender, EventArgs e)
         {
