45832f31952455da0c26fc438fa15d79894c7550
muril0ve <murilo.coutinho@aluno.ifsp.edu.br>
2023-11-27 21:58:08 -0300

    a

--- Forms1_/editlogin.cs
+++ Forms1_/editlogin.cs
@@ -1,19 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using iTextSharp;
 using iTextSharp.text;
 using iTextSharp.text.pdf;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 using System.IO;
 using System.Data.SqlClient;
 using InvestimentosMais;
+
+
 
 namespace Forms1_
 {
@@ -131,15 +125,7 @@ namespace Forms1_
             Close();
         }
 
-        private void label7_Click(object sender, EventArgs e)
-        {
 
-        }
-
-        private void senha_TextChanged(object sender, EventArgs e)
-        {
-
-        }
 
         private void nome_TextChanged(object sender, EventArgs e)
         {
@@ -150,19 +136,7 @@ namespace Forms1_
         {
 
         }
-        private static bool VerificarUsuarioExistenteAddress(string nomeUsuario)
-        {
-            connection connection = new connection();
-            SqlCommand sqlCommand = new SqlCommand();
-            sqlCommand.Connection = connection.ReturnConnection();
-            sqlCommand.CommandText = "SELECT COUNT(*) FROM login WHERE email = @email";
 
-            sqlCommand.Parameters.AddWithValue("@verificar", nomeUsuario);
-
-            int count = (int)sqlCommand.ExecuteScalar();
-
-            return count > 0;
-        }
 
         private void email_TextChanged(object sender, EventArgs e)
         {
@@ -170,101 +144,53 @@ namespace Forms1_
         }
 
         private void imprimir_Click(object sender, EventArgs e)
-
         {
-            string nomeUsuario = Nome.Text;
-            bool loginExiste = VerificarUsuarioExistenteAddress(nomeUsuario);
+            string connectionString = "DESKTOP-MBDNUGB\\SQLEXPRESS"; // Substitua pela sua string de conexão
+            string query = "SELECT * FROM login";
 
-            for (; ; )
+            using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                if (loginExiste)
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand(query, connection))
                 {
-                    string email, senha, nome, cpf;
-
-                    connection connection = new connection();
-                    SqlCommand sqlCommand = new SqlCommand();
-
-                    sqlCommand.Connection = connection.ReturnConnection();
-                    sqlCommand.CommandText = "SELECT * FROM CreateAccount WHERE usuario = @user";
-                    sqlCommand.Parameters.AddWithValue("@user", Nome.Text);
-
-                    if (Nome.Text == "")
+                    using (SqlDataReader reader = command.ExecuteReader())
                     {
-                        MessageBox.Show(
-                        "preencha o nome de usuário!",
-                        "MONDIAL™",
-                        MessageBoxButtons.OK,
-                        MessageBoxIcon.Information);
-                        Nome.Clear();
-                    }
-                    else
-                    {
-                        using (SqlDataReader reader = sqlCommand.ExecuteReader())
+                        using (FileStream fs = new FileStream("relatorio.pdf", FileMode.Create))
                         {
-                            reader.Read();
-                            email = (string)reader["email"];
-                            senha = "**";
-                            nome = (string)reader["nome"];
-                            ;
+                            using (Document doc = new Document())
+                            {
+                                using (PdfWriter pdfWriter = PdfWriter.GetInstance(doc, fs))
+                                {
+                                    doc.Open();
+
+                                    while (reader.Read())
+                                    {
+                                        string email = reader["email"].ToString();
+                                        string senha = "***"; // Substitua pelo valor que você deseja exibir para senha
+                                        string nome = reader["nome"].ToString();
+                                        string cpf = reader["cpf"].ToString();
+
+                                        // Correção: Use o iTextSharp.text.Paragraph ao invés de System.Windows.Forms.Paragraph
+                                        iTextSharp.text.Paragraph paragraph = new iTextSharp.text.Paragraph();
+                                        paragraph.Add("Email: " + email + "\n");
+                                        paragraph.Add("Senha: " + senha + "\n");
+                                        paragraph.Add("Nome: " + nome + "\n");
+                                        paragraph.Add("CPF: " + cpf + "\n\n");
+
+                                        doc.Add(paragraph);
+                                    }
+                                }
+                            }
                         }
-
-                        sqlCommand.CommandText = "SELECT * FROM login WHERE usuario = @user";
-
-                        using (SqlDataReader reader1 = sqlCommand.ExecuteReader())
-                        {
-                            reader1.Read();
-
-                            email = (string)reader1["email"];
-                            senha = (string)reader1["senha"];
-                            nome = (string)reader1["nome"];
-                            cpf = (string)reader1["cpf"];
-
-
-                        }
-                        string caminho = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-                        string archivename = "relatorio_" + Nome.Text + ".pdf";
-                        string caminhoCompleto = Path.Combine(caminho, archivename);
-                        FileStream archivePDF = new FileStream(caminhoCompleto, FileMode.Create);
-                        Document doc = new Document(PageSize.A4);
-                        PdfWriter pdfwriter = PdfWriter.GetInstance(doc, archivePDF);
-
-                        string dados = "";
-
-                        Paragraph paragraph = new Paragraph(dados, new iTextSharp.text.Font(iTextSharp.text.Font.FontFamily.TIMES_ROMAN, 12, (int)System.Drawing.FontStyle.Bold));
-                        paragraph.Alignment = Element.ALIGN_CENTER;
-                        paragraph.Add("RELATÓRIO - MONDIAL");
-
-                        Paragraph paragraphuser = new Paragraph(dados, new iTextSharp.text.Font(iTextSharp.text.Font.FontFamily.TIMES_ROMAN, 12, (int)System.Drawing.FontStyle.Regular));
-                        paragraphuser.Alignment = Element.ALIGN_CENTER;
-                        paragraphuser.Add("Dados do Usuário:\n" + "Usuário: " + "\nSenha: " + senha + "\n CPF: " + cpf + "\nE-mail: " + email);
-
-
-                        doc.Open();
-                        doc.Add(paragraph);
-                        doc.Add(paragraphuser);
-
-                        doc.Close();
-
-                        MessageBox.Show(
-                        "O relatório foi criado com sucesso e armazenado na sua área de trabalho!",
-                        "MONDIAL™",
-                        MessageBoxButtons.OK,
-                        MessageBoxIcon.Information);
-                        break;
                     }
-                }
-                else
-                {
-                    MessageBox.Show(
-                    "O nome de usuário é inválido!",
-                    "MONDIAL™",
-                    MessageBoxButtons.OK,
-                    MessageBoxIcon.Information);
-                    break;
-
                 }
             }
+
+            MessageBox.Show("PDF gerado com sucesso.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
+
+
 
